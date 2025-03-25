6c34b81dde96b81b8a49645cec08325784f6a763
Hemesh Khilani <Hemesh.Khilani@blackbaud.me>
2025-03-25 20:42:48 +0530

    Added QuestionAnswerController for Azure Q&A service integration
    Introduced QuestionAnswerController in CustomerSentimentAnalysis.Controllers namespace to handle question-answering functionality using Azure's Question Answering service. The controller includes a GetAnswer method to process POST requests, query the Azure service, and return answers. Added necessary using directives for Azure and ASP.NET Core MVC.

---
+++ BackEnd/CustomerSentimentAnalysis/Controllers/QuestionAnswerController.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.AI.Language.QuestionAnswering;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerSentimentAnalysis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionAnswerController : ControllerBase
+    {
+        private readonly string endpoint = "https://otg-lang.cognitiveservices.azure.com/";
+        private readonly string apiKey = "9C6EUFScvrHWOJfBuyMbZs3hmky2usIMOZvyJIBDA2oLAdtknz4CJQQJ99BCACYeBjFXJ3w3AAAaACOGAoPT";
+        private readonly string projectName = "OTG-Question-Answering"; 
+        private readonly string deploymentName = "production"; 
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> GetAnswer([FromBody] string question)
+        {
+            var client = new QuestionAnsweringClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+            var project = new QuestionAnsweringProject(projectName, deploymentName);
+            var answer = await client.GetAnswersAsync(question, project);
+            return Ok(new { answer });
+        }
+    }
+}

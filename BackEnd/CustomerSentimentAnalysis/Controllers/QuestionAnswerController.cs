6ae1cd74da0c4f681b904c065b7165f4136cd736
Hemesh Khilani <Hemesh.Khilani@blackbaud.me>
2025-03-26 13:27:16 +0530

    Refactored GetAnswer method and add confidence check
    - Added `using static NuGet.Packaging.PackagingConstants;` directive.
    - Changed `GetAnswer` method's parameter from `string` to `QuestionAnswerRequest`.
    - Updated `GetAnswer` to use `request.Question` instead of `question`.
    - Added confidence check in `GetAnswer`, returning `NotFound` if confidence < 0.4.
    - Introduced `QuestionAnswerRequest` class with `Question` property and `ToJson` method.

--- BackEnd/CustomerSentimentAnalysis/Controllers/QuestionAnswerController.cs
+++ BackEnd/CustomerSentimentAnalysis/Controllers/QuestionAnswerController.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using Azure.AI.Language.QuestionAnswering;
 using Microsoft.AspNetCore.Mvc;
+using static NuGet.Packaging.PackagingConstants;
 
 namespace CustomerSentimentAnalysis.Controllers
 {
@@ -15,12 +16,22 @@ namespace CustomerSentimentAnalysis.Controllers
 
         [HttpPost]
         [Route("")]
-        public async Task<IActionResult> GetAnswer([FromBody] string question)
+        public async Task<IActionResult> GetAnswer([FromBody] QuestionAnswerRequest request)
         {
             var client = new QuestionAnsweringClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
             var project = new QuestionAnsweringProject(projectName, deploymentName);
-            var answer = await client.GetAnswersAsync(question, project);
+            var answer = await client.GetAnswersAsync(request.Question, project);
+            if (answer.Value.Answers[0].Confidence < 0.4) return NotFound();
             return Ok(new { answer });
+        }
+
+        public class QuestionAnswerRequest
+        {
+            public string Question { get; set; }
+            public string ToJson()
+            {
+                return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            }
         }
     }
 }
